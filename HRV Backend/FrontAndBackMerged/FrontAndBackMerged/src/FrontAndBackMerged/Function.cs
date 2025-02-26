3308de46b6bbee93c9315f632c42b6f3e94b2738
Brady Dullanty <bdullanty@ewu.edu>
2025-02-25 17:51:07 -0800

    Added StoreHeartBeats, StoreTimesection functionality

--- HRV Backend/FrontAndBackMerged/FrontAndBackMerged/src/FrontAndBackMerged/Function.cs
+++ HRV Backend/FrontAndBackMerged/FrontAndBackMerged/src/FrontAndBackMerged/Function.cs
@@ -20,7 +20,7 @@ public class Function
     public string? RequestType { get; set; }
     public string? AccessKey;
     public string? AnonymizedID;
-
+    public string? HeartRateDataID;
     public string FunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
     {
         try
@@ -203,7 +203,228 @@ public class Function
 
     public bool AddData()
     {
-        throw new NotImplementedException();
+        //Store Heartbeats
+        if (!StoreHeartBeats()) return false;
+
+        if (!StoreTimeSection()) return false;
+        //Store Time Section Data
+        return true;
+    }
+
+    private bool StoreTimeSection()
+    {
+        
+
+        try
+        {
+            (string? questionaireID, string? medicationsID) = getUserDataIDS();
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine("DateTime:" + dateTime.ToString());
+
+            JsonDocument doc = JsonDocument.Parse(Request!.Body);
+            string body = doc.RootElement.GetProperty("body").ToString();
+            JsonDocument bodyDoc = JsonDocument.Parse(body);
+            string requestDataString = bodyDoc.RootElement.GetProperty("RequestData").ToString();
+            JsonDocument requestData = JsonDocument.Parse(requestDataString);
+            //get hrv info
+            string hrvInfoString = requestData.RootElement.GetProperty("HRVInfo").ToString();
+            JsonDocument hrvInfoJson = JsonDocument.Parse(hrvInfoString);
+            double sdnn = hrvInfoJson.RootElement.GetProperty("SDNN").GetDouble();
+            double rmssd = hrvInfoJson.RootElement.GetProperty("RMSSD").GetDouble();
+            double pnn50 = hrvInfoJson.RootElement.GetProperty("PNN50").GetDouble();
+           
+            //get flags
+            string flagsString = requestData.RootElement.GetProperty("Flags").ToString();
+            JsonDocument Flags = JsonDocument.Parse(flagsString);
+            bool userFlagged = Flags.RootElement.GetProperty("UserFlagged").GetBoolean();
+            bool programFlagged = Flags.RootElement.GetProperty("ProgramFlagged").GetBoolean();
+            //get personal Data
+            string personalDataString = requestData.RootElement.GetProperty("PersonalData").ToString();
+            JsonDocument personalDataJson = JsonDocument.Parse(personalDataString);
+            string injuryString = personalDataJson.RootElement.GetProperty("Injury").ToString();
+            JsonDocument InjuryDataJson = JsonDocument.Parse(injuryString);
+            int age = ((int)personalDataJson.RootElement.GetProperty("Age").GetDouble());
+            double bmi = personalDataJson.RootElement.GetProperty("BMI").GetDouble();
+            string gender = personalDataJson.RootElement.GetProperty("Gender").ToString();
+            string hospitalName = personalDataJson.RootElement.GetProperty("HospitalName").ToString();
+            string injuryType = InjuryDataJson.RootElement.GetProperty("Type").ToString();
+            string injuryDate = InjuryDataJson.RootElement.GetProperty("Date").ToString();
+            string timeSectionID = Guid.NewGuid().ToString();
+            Console.WriteLine(injuryDate);
+
+            string insertQuery = "INSERT INTO `TimeSectionDetails` (`AnonymizedID`, `TimeSectionID`, `CreatedAt`, `HeartRateDataID`, `SDNN`, `RMSSD`, `PNN50`, `MedicationsID`, `UserFlagged`, `ProgramFlagged`, `Gender`, `BMI`, `Age`, `HospitalName`, `InjuryType`, `DateOfInjury`, `QuestionnaireID`) " +
+                         "VALUES (@AnonymizedID, @TimeSectionID, @CreatedAt, @HeartRateDataID, @SDNN, @RMSSD, @PNN50, @MedicationsID, @UserFlagged, @ProgramFlagged, @Gender, @BMI, @Age, @HospitalName, @InjuryType, @DateOfInjury, @QuestionaireID)";
+
+            // List to hold parameters
+            var parameters = new List<MySqlParameter>
+            {
+                new MySqlParameter("@AnonymizedID", MySqlDbType.VarChar) { Value =  AnonymizedID},
+                new MySqlParameter("@TimeSectionID", MySqlDbType.VarChar) { Value = timeSectionID },
+                new MySqlParameter("@CreatedAt", MySqlDbType.DateTime) { Value = dateTime },
+                new MySqlParameter("@HeartRateDataID", MySqlDbType.VarChar) { Value = HeartRateDataID },
+                new MySqlParameter("@SDNN", MySqlDbType.Double) { Value = sdnn },
+                new MySqlParameter("@RMSSD", MySqlDbType.Double) { Value = rmssd },
+                new MySqlParameter("@PNN50", MySqlDbType.Double) { Value = pnn50 },
+                new MySqlParameter("@MedicationsID", MySqlDbType.VarChar) { Value = medicationsID },
+                new MySqlParameter("@UserFlagged", MySqlDbType.Bit) { Value = userFlagged },
+                new MySqlParameter("@ProgramFlagged", MySqlDbType.Bit) { Value = programFlagged },
+                new MySqlParameter("@Gender", MySqlDbType.VarChar) { Value = gender }, // Modify to extract gender
+                new MySqlParameter("@BMI", MySqlDbType.Double) { Value = bmi },
+                new MySqlParameter("@Age", MySqlDbType.Int32) { Value = age },
+                new MySqlParameter("@HospitalName", MySqlDbType.VarChar) { Value = hospitalName },
+                new MySqlParameter("@InjuryType", MySqlDbType.VarChar) { Value = injuryType },
+                new MySqlParameter("@DateOfInjury", MySqlDbType.DateTime) { Value = injuryDate},
+                new MySqlParameter("@QuestionaireID", MySqlDbType.VarChar) { Value = questionaireID }
+             };
+
+            //Now we send:
+            using (MySqlCommand command = new MySqlCommand(insertQuery, Connection))
+            {
+                command.Parameters.AddRange(parameters.ToArray());
+                int rowsAffected = command.ExecuteNonQuery();
+
+
+                int expectedRowsAffected = 1;
+
+                if (rowsAffected == expectedRowsAffected)
+                {
+                    Console.WriteLine($"{rowsAffected} heartbeats added to database");
+                    return true;
+                }
+                else
+                {
+                    Console.Write($"{rowsAffected} changed when expected {expectedRowsAffected}");
+                    Console.WriteLine("Failure writing TimeSectionData to database");
+                    return false;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Unexpected Error, failure write TimeSection data to database: " + ex);
+            return false;
+        }
+
+
+    }
+    private (string? medicationsID, string? questionnaireID) getUserDataIDS()
+    {
+        // SQL query to select both CurrentMedicationsID and QuestionnaireID
+        string query = "SELECT CurrentMedicationsID, QuestionnaireID FROM UserData WHERE AnonymizedID = @AnonymizedID AND AccessKey = @AccessKey LIMIT 1";
+
+        // Open connection to the database
+        try
+        {
+
+            // Create the command and add parameters to avoid SQL injection
+            using (var command = new MySqlCommand(query, Connection))
+            {
+                command.Parameters.AddWithValue("@AnonymizedID", AnonymizedID);
+                command.Parameters.AddWithValue("@AccessKey", AccessKey);
+
+                // Execute the query and retrieve both values
+                using (var reader = command.ExecuteReader())
+                {
+                    // If a row is found, read the values
+                    if (reader.Read())
+                    {
+                        string? medicationsID = reader.IsDBNull(reader.GetOrdinal("CurrentMedicationsID")) ? null : reader.GetString(reader.GetOrdinal("CurrentMedicationsID"));
+                        string? questionnaireID = reader.IsDBNull(reader.GetOrdinal("QuestionnaireID")) ? null : reader.GetString(reader.GetOrdinal("QuestionnaireID"));
+
+                        return (medicationsID, questionnaireID);
+                    }
+                    else
+                    {
+                        // If no data found, return null values
+                        return (null, null);
+                    }
+                }
+            }
+        }
+        catch (Exception ex) {
+            Console.WriteLine($"Failed to get Medication and questionaireID {ex}");
+            return (null,null);
+        }
+    }
+    private double[] GetHeartBeatsFromJson()
+    {
+        JsonDocument doc = JsonDocument.Parse(Request!.Body);
+        string body = doc.RootElement.GetProperty("body").ToString();
+        JsonDocument bodyDoc = JsonDocument.Parse(body);
+        string requestDataString = bodyDoc.RootElement.GetProperty("RequestData").ToString();
+        JsonDocument requestData = JsonDocument.Parse(requestDataString);
+        string hrvInfoString = requestData.RootElement.GetProperty("HRVInfo").ToString();
+        JsonDocument hrvInfoJson = JsonDocument.Parse(hrvInfoString);
+        JsonElement.ArrayEnumerator heartBeatEnumerator = hrvInfoJson.RootElement.GetProperty("HeartBeats").EnumerateArray();
+        List<double> heartBeatList = new();
+        foreach (var item in heartBeatEnumerator)
+        {
+            //Console.WriteLine("Added:" + item.GetDouble());
+            heartBeatList.Add(item.GetDouble());
+        }
+        return heartBeatList.ToArray();
+    }
+
+    private bool StoreHeartBeats()
+    {
+        try
+        {
+            string heartRateDataID = Guid.NewGuid().ToString();
+            HeartRateDataID = heartRateDataID;
+            string heartRateDataIDParam = "@HeartBeatDataID";
+            double[] heartBeats = GetHeartBeatsFromJson();
+
+
+            string insertQuery = "INSERT INTO `HeartRateData` (`HeartRateDataID`, `Time`, `BPM`) VALUES ";
+
+            // Create a list to hold the parameters for the medications
+            var parameters = new List<MySqlParameter>();
+            parameters.Add(new MySqlParameter(heartRateDataIDParam, heartRateDataID));
+
+            for (int i = 0; i < heartBeats.Length; i++)
+            {
+                // Create unique parameter names for each medication entry
+                string HeartBeatParamName = $"@BPM{i}";
+                insertQuery += $"({heartRateDataIDParam}, {i + 1}, {HeartBeatParamName})";
+
+                // Add the parameter to the parameters list
+                parameters.Add(new MySqlParameter(HeartBeatParamName, heartBeats[i]));
+
+                // If it's not the last entry, add a comma for the next row
+                if (i < heartBeats.Length - 1)
+                {
+                    insertQuery += ", ";
+                }
+            }
+
+
+            //Now we send:
+            using (MySqlCommand command = new MySqlCommand(insertQuery, Connection))
+            {
+                command.Parameters.AddRange(parameters.ToArray());
+                int rowsAffected = command.ExecuteNonQuery();
+
+
+                int expectedRowsAffected = heartBeats.Length;
+
+                if (rowsAffected == expectedRowsAffected)
+                {
+                    Console.WriteLine($"{rowsAffected} heartbeats added to database");
+                    return true;
+                }
+                else
+                {
+                    Console.Write($"{rowsAffected} changed when expected {expectedRowsAffected}");
+                    Console.WriteLine("Failure writing heartbeats to database");
+                    return false;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Unexpected Error, failure write heartbeat data to database: " + ex);
+            return false;
+        }
     }
 
     public bool UpdateMedications()
@@ -247,7 +468,6 @@ public class Function
             using (MySqlCommand command = new MySqlCommand(finalQuery, Connection))
             {
                 command.Parameters.AddRange(parameters.ToArray());
-                PrintExecutingQuery(command);
                 int rowsAffected = command.ExecuteNonQuery();
 
 
@@ -266,9 +486,9 @@ public class Function
                 return true;
             }
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
-            Console.WriteLine("Unexpected Error, failure to write to database: "+ex);
+            Console.WriteLine("Unexpected Error, failure to write to database: " + ex);
             return false;
         }
 
